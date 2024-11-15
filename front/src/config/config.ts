4b7e9d3f2e04aeed049aaf4a74a839931937cb11
Élyse Viard <elysasrc@proton.me>
2024-11-15 12:28:57 +0100

    front: support using front from outside the reverse proxy
    Signed-off-by: Élyse Viard <elysasrc@proton.me>

--- front/src/config/config.ts
+++ front/src/config/config.ts
@@ -1,6 +1,6 @@
 export const MAIN_API = {
-  proxy_editoast: '/api',
-  proxy_gateway: '',
+  proxy_editoast: import.meta.env.VITE_PROXY_EDITOAST || '/api',
+  proxy_gateway: import.meta.env.VITE_PROXY_GATEWAY || '',
   version: '0.0.1',
   editor: {
     component_identifier: { database: 'gaia', name: 'Test' },
