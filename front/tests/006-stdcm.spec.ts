eaa4315a038e33130f9bba24953d3a84207552a4
SharglutDev <p.filimon75@gmail.com>
2024-12-03 16:12:04 +0100

    front: animate new intermediate op in stdcm
    - Add an animation when a new intermediate op is added
    - Scroll to keep it in the viewport if it overflows
    - Adapat e2e tests in stdcm to ensure buttons are clickable while it's animating
    
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

--- front/tests/006-stdcm.spec.ts
+++ front/tests/006-stdcm.spec.ts
@@ -14,6 +14,8 @@ test.use({
 test.describe('Verify train schedule elements and filters', () => {
   test.slow(); // Mark test as slow due to multiple steps
 
+  test.use({ viewport: { width: 1920, height: 1080 } });
+
   let infra: Infra;
   let OSRDLanguage: string;
 
