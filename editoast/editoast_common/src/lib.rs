ab0441c38a6b9cfd2f7d317de8a525edd1a295ce
Tristram Gräbener <tristram+git@tristramg.eu>
2024-12-07 14:31:03 +0100

    editoast: common: define units to allow to SerDe uom quantities
    Signed-off-by: Tristram Gräbener <tristram+git@tristramg.eu>

--- editoast/editoast_common/src/lib.rs
+++ editoast/editoast_common/src/lib.rs
@@ -3,6 +3,7 @@ mod hash_rounded_float;
 pub mod rangemap_utils;
 pub mod schemas;
 pub mod tracing;
+pub mod units;
 
 pub use hash_rounded_float::hash_float;
 pub use hash_rounded_float::hash_float_slice;
