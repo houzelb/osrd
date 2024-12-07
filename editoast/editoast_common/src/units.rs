5432c6c5c7c1f386986fc97633a9bfddf05ec8b2
Tristram Gräbener <tristram+git@tristramg.eu>
2024-12-07 14:31:03 +0100

    editoast: common: define units to allow to SerDe uom quantities
    Signed-off-by: Tristram Gräbener <tristram+git@tristramg.eu>

---
+++ editoast/editoast_common/src/units.rs
@@ -0,0 +1,164 @@
+//! Module to allow the use of serde with uom quantities
+//!
+//! The serde feature of uom doesn’t allow to specify in which unit the value will be serialized.
+//!
+//! Two helpers are provided for convenience:
+//! * `unit::new` (e.g. `meter::new(32)`) to build a new quantity from a f64 in the given unit
+//! * `unit::from` (e.g. `millimeter::from(length)`) to have the quantity as f64 in the given unit
+//!
+//! ## Usage
+//!
+//! ```ignore
+//! use editoast_model::units::*;
+//! #[derive(Debug, Serialize, Derivative)]
+//! struct Train {
+//!     // This means that serde with read and write the velocity in meters per second
+//!     #[serde(with="meter_per_second")]
+//!     max_speed: Velocity,
+//!     // When using optional values, we must add `default` and use ::option unit
+//!     // See https://stackoverflow.com/a/44303505
+//!     #[serde(default, with="meter::option")]
+//!     length: Option<Length>,
+//! }
+//!
+//! impl Train {
+//!     fn from_meter_per_seconds(mps: f64) -> Self {
+//!         Self {
+//!             max_speed: meter_per_second::new(mps),
+//!         }
+//!     }
+//!
+//!     fn print(&self) {
+//!         println!("The max speed is: {} km/h", kilometer_per_hour::from(self.max_speed));
+//!     }
+//! }
+//! ```
+
+/// Re-export the Quantities that are used in OSRD
+pub use uom::si::f64::{
+    Acceleration, Force, Frequency, Length, LinearMassDensity, LinearNumberDensity, Mass, MassRate,
+    Time, Velocity,
+};
+
+macro_rules! quantity_to_path {
+    (Length, $unit:ident) => {
+        uom::si::length::$unit
+    };
+    (Velocity, $unit:ident) => {
+        uom::si::velocity::$unit
+    };
+    (Acceleration, $unit:ident) => {
+        uom::si::acceleration::$unit
+    };
+    (Mass, $unit:ident) => {
+        uom::si::mass::$unit
+    };
+    (Force, $unit:ident) => {
+        uom::si::force::$unit
+    };
+    (MassRate, $unit:ident) => {
+        uom::si::mass_rate::$unit
+    };
+    (Frequency, $unit:ident) => {
+        uom::si::frequency::$unit
+    };
+    (LinearMassDensity, $unit:ident) => {
+        uom::si::linear_mass_density::$unit
+    };
+    (LinearNumberDensity, $unit:ident) => {
+        uom::si::linear_number_density::$unit
+    };
+    (Time, $unit:ident) => {
+        uom::si::time::$unit
+    };
+    (Ratio, $unit:ident) => {
+        uom::si::ratio::$unit
+    };
+}
+
+macro_rules! define_unit {
+    ($unit:ident, $quantity:ident) => {
+        pub mod $unit {
+            use serde::{Deserialize, Deserializer, Serialize, Serializer};
+            use uom::si::f64::*;
+            type Unit = quantity_to_path!($quantity, $unit);
+            pub type ReprType = f64;
+
+            pub fn serialize<S>(value: &$quantity, serializer: S) -> Result<S::Ok, S::Error>
+            where
+                S: Serializer,
+            {
+                value.get::<Unit>().serialize(serializer)
+            }
+
+            pub fn deserialize<'de, D>(deserializer: D) -> Result<$quantity, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                let value = ReprType::deserialize(deserializer)?;
+                Ok($quantity::new::<Unit>(value))
+            }
+
+            pub fn new(value: ReprType) -> $quantity {
+                $quantity::new::<Unit>(value)
+            }
+
+            pub fn from(qty: $quantity) -> ReprType {
+                qty.get::<Unit>()
+            }
+
+            pub fn hash<H: std::hash::Hasher>(value: &$quantity, state: &mut H) {
+                crate::hash_float::<5, H>(&from(*value), state);
+            }
+
+            pub mod option {
+                use super::*;
+                pub type ReprType = Option<super::ReprType>;
+
+                pub fn serialize<S>(
+                    value: &Option<$quantity>,
+                    serializer: S,
+                ) -> Result<S::Ok, S::Error>
+                where
+                    S: Serializer,
+                {
+                    value.map(|value| value.get::<Unit>()).serialize(serializer)
+                }
+
+                pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<$quantity>, D::Error>
+                where
+                    D: Deserializer<'de>,
+                {
+                    let value = Option::deserialize(deserializer)?;
+                    Ok(value.map(|value| $quantity::new::<Unit>(value)))
+                }
+
+                pub fn new(value: ReprType) -> Option<$quantity> {
+                    value.map(|v| $quantity::new::<Unit>(v))
+                }
+
+                pub fn from(qty: Option<$quantity>) -> ReprType {
+                    qty.map(|q| q.get::<Unit>())
+                }
+
+                pub fn hash<H: std::hash::Hasher>(value: &Option<$quantity>, state: &mut H) {
+                    super::hash(&value.unwrap_or_default(), state);
+                }
+            }
+        }
+    };
+}
+
+// Any new value here must also be added in editoast_derive/src/annotate_units.rs
+define_unit!(meter, Length);
+define_unit!(millimeter, Length);
+define_unit!(meter_per_second, Velocity);
+define_unit!(meter_per_second_squared, Acceleration);
+define_unit!(kilogram, Mass);
+define_unit!(newton, Force);
+define_unit!(kilogram_per_second, MassRate);
+define_unit!(hertz, Frequency);
+define_unit!(kilogram_per_meter, LinearMassDensity);
+define_unit!(per_meter, LinearNumberDensity);
+define_unit!(second, Time);
+define_unit!(millisecond, Time);
