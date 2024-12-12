84fddc6afd42f93b1bfa07d596a6e8af7aa35d75
SharglutDev <p.filimon75@gmail.com>
2024-12-12 08:20:20 +0100

    front: add disabled style to osrd menu items
    Signed-off-by: SharglutDev <p.filimon75@gmail.com>

--- front/src/common/OSRDMenu.tsx
+++ front/src/common/OSRDMenu.tsx
@@ -4,6 +4,8 @@ export type OSRDMenuItem = {
   title: string;
   icon: React.ReactNode;
   onClick: () => void;
+  disabled?: boolean;
+  disabledMessage?: string;
 };
 
 type OSRDMenuProps = {
@@ -13,8 +15,15 @@ type OSRDMenuProps = {
 
 const OSRDMenu = ({ menuRef, items }: OSRDMenuProps) => (
   <div ref={menuRef} className="osrd-menu">
-    {items.map(({ title, icon, onClick }) => (
-      <button key={title} type="button" className="menu-item" onClick={onClick}>
+    {items.map(({ title, icon, disabled, disabledMessage, onClick }) => (
+      <button
+        disabled={disabled}
+        title={disabled ? disabledMessage : undefined}
+        key={title}
+        type="button"
+        className="menu-item"
+        onClick={onClick}
+      >
         <span className="icon">{icon}</span>
         <span>{title}</span>
       </button>
