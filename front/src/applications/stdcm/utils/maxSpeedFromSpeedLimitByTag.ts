55139f933e2974b5b45555e9772b589fce2de04c
Egor Berezovskiy <egor@berezify.fr>
2024-12-16 10:58:44 +0100

    front: fix pr comments
    Signed-off-by: Egor Berezovskiy <egor@berezify.fr>

--- front/src/applications/stdcm/utils/maxSpeedFromSpeedLimitByTag.ts
+++ front/src/applications/stdcm/utils/maxSpeedFromSpeedLimitByTag.ts
@@ -1,3 +1,5 @@
+import { COMPOSITION_CODES_MAX_SPEEDS } from '../consts';
+
 /**
  * Get max speed in km/h from speedLimitByTag
  * @param speedLimitByTag - A string representing the speed limit tag
@@ -5,20 +7,7 @@
  */
 const maxSpeedFromSpeedLimitByTag = (speedLimitByTag?: string | null): number | undefined => {
   if (!speedLimitByTag) return undefined;
-
-  const speedMap: Record<string, number | undefined> = {
-    MA80: 80,
-    MA90: 90,
-    MA100: 100,
-    ME100: 100,
-    ME120: 120,
-    ME140: 140,
-    ME160: 160,
-    HLP: undefined,
-    MVGV: undefined,
-  };
-
-  return speedMap[speedLimitByTag] ?? undefined;
+  return COMPOSITION_CODES_MAX_SPEEDS[speedLimitByTag] ?? undefined;
 };
 
 export default maxSpeedFromSpeedLimitByTag;
