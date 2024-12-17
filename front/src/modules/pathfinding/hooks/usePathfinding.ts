4f7dd40b24556af6037d8fd7061246e7554ef143
Simon Ser <contact@emersion.fr>
2024-12-17 10:11:58 +0100

    front: drop pathfinding error message bubble
    The "pathfindingError" translation needs an "errorMessage" property,
    so it's not displayed properly.
    
    Since the error is already displayed to the user in a different way,
    we can just drop the bubble.
    
    To reproduce:
    
    - Select a train in small_infra
    - Switch to another infra (e.g. France)
    - Open the "Create new train" modal
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/modules/pathfinding/hooks/usePathfinding.ts
+++ front/src/modules/pathfinding/hooks/usePathfinding.ts
@@ -90,7 +90,6 @@ const usePathfinding = (
       launchPathfinding(updatedPathSteps);
     } else {
       setError(t('missingPathSteps'));
-      dispatch(setFailure({ name: t('pathfindingError'), message: t('missingPathSteps') }));
     }
   };
 
