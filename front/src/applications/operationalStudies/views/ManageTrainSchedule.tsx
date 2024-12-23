661b5fc56623db0c0c3605c15b95a1ada3ef1e25
Simon Ser <contact@emersion.fr>
2024-12-23 16:31:23 +0100

    front: move allWaypoints to ManageTrainScheduleContextType
    This value should be updated every time pathSteps or
    suggestedOperationalPoints change. Instead of manually updating it
    when touching these two values, memoize it.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/applications/operationalStudies/views/ManageTrainSchedule.tsx
+++ front/src/applications/operationalStudies/views/ManageTrainSchedule.tsx
@@ -35,7 +35,7 @@ type ManageTrainScheduleProps = {
 
 const ManageTrainSchedule = ({ trainIdToEdit }: ManageTrainScheduleProps) => {
   const { t } = useTranslation(['operationalStudies/manageTrainSchedule']);
-  const { pathProperties, voltageRanges } = useManageTrainScheduleContext();
+  const { pathProperties, voltageRanges, allWaypoints } = useManageTrainScheduleContext();
 
   const { getOrigin, getDestination, getPathSteps } = useOsrdConfSelectors();
   const origin = useSelector(getOrigin);
@@ -120,7 +120,7 @@ const ManageTrainSchedule = ({ trainIdToEdit }: ManageTrainScheduleProps) => {
     // If pathProperties is defined we know that pathSteps won't have any null values
     content: (
       <TimesStopsInput
-        allWaypoints={pathProperties?.allWaypoints}
+        allWaypoints={allWaypoints}
         startTime={new Date(startTime)}
         pathSteps={compact(pathSteps)}
       />
