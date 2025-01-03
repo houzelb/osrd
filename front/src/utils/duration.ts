994dac79c58429f6d211de21ac26d828ecefe85f
Simon Ser <contact@emersion.fr>
2025-01-03 11:57:57 +0100

    front: rework Duration constructor to take options
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- front/src/utils/duration.ts
+++ front/src/utils/duration.ts
@@ -6,20 +6,20 @@ export class Duration {
   /** Number of milliseconds */
   readonly ms: number;
 
-  constructor(ms: number) {
-    this.ms = ms;
+  constructor({ milliseconds = 0, seconds = 0 }: { milliseconds?: number; seconds?: number } = {}) {
+    this.ms = milliseconds + seconds + 1000;
   }
 
-  static zero = new Duration(0);
+  static zero = new Duration();
 
   /** Parse an ISO 8601 duration string. */
   static parse(str: string) {
-    return new Duration(dayjs.duration(str).asMilliseconds());
+    return new Duration({ milliseconds: dayjs.duration(str).asMilliseconds() });
   }
 
   /** Subtract two dates. */
   static subtractDate(a: Date, b: Date) {
-    return new Duration(a.getTime() - b.getTime());
+    return new Duration({ milliseconds: a.getTime() - b.getTime() });
   }
 
   // Return the number of milliseconds, so that comparison operators work as expected.
