2d7acaab2a9e1fadd82d91279fcfa46d55e06830
Leo Valais <leo.valais97@gmail.com>
2025-01-08 17:43:09 +0100

    fixup! editoast: change Model trait DeleteStatic to use editoast_models::Error
    Signed-off-by: Leo Valais <leo.valais97@gmail.com>

--- editoast/src/models/timetable.rs
+++ editoast/src/models/timetable.rs
@@ -60,12 +60,11 @@ impl Timetable {
 
 #[async_trait::async_trait]
 impl DeleteStatic<i64> for Timetable {
-    #[allow(clippy::blocks_in_conditions)] // TODO: Remove this once using clippy 0.1.80
     #[tracing::instrument(name = "model:delete_static<Timetable>", skip_all, ret, err)]
     async fn delete_static(
         conn: &mut DbConnection,
         id: i64,
-    ) -> std::result::Result<bool, editoast_models::model::Error> {
+    ) -> Result<bool, editoast_models::model::Error> {
         let n = diesel::delete(dsl::timetable.filter(dsl::id.eq(id)))
             .execute(conn.write().await.deref_mut())
             .await?;
@@ -75,7 +74,6 @@ impl DeleteStatic<i64> for Timetable {
 
 #[async_trait::async_trait]
 impl Retrieve<i64> for Timetable {
-    #[allow(clippy::blocks_in_conditions)] // TODO: Remove this once using clippy 0.1.80
     #[tracing::instrument(name = "model:retrieve<Timetable>", skip_all, err)]
     async fn retrieve(
         conn: &mut editoast_models::DbConnection,
@@ -92,7 +90,6 @@ impl Retrieve<i64> for Timetable {
 
 #[async_trait::async_trait]
 impl Exists<i64> for Timetable {
-    #[allow(clippy::blocks_in_conditions)] // TODO: Remove this once using clippy 0.1.80
     #[tracing::instrument(name = "model:exists<Timetable>", skip_all, ret, err)]
     async fn exists(conn: &mut DbConnection, id: i64) -> Result<bool> {
         Self::retrieve(conn, id)
