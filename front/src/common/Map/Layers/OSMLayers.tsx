f43c540f55a771034660f7ae23b1cdcd6d0838bb
Clara Ni <clara.ni@outlook.fr>
2025-01-10 16:33:44 +0100

    front: group layers related to OSM in OSMLayers
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

---
+++ front/src/common/Map/Layers/OSMLayers.tsx
@@ -0,0 +1,54 @@
+import { LAYER_GROUPS_ORDER, LAYERS } from 'config/layerOrder';
+
+import Background from './Background';
+import Hillshade from './Hillshade';
+import OSM from './OSM';
+import PlatformsLayer from './Platforms';
+import Terrain from './Terrain';
+import TracksOSM from './TracksOSM';
+import colors from '../Consts/colors';
+
+type OSMLayersProps = {
+  mapStyle: 'normal' | 'dark' | 'blueprint' | 'minimal';
+  showOSM: boolean;
+  mapIsLoaded?: boolean;
+  hidePlatforms?: boolean;
+};
+
+const OSMLayers = ({
+  mapStyle,
+  showOSM,
+  mapIsLoaded = true,
+  hidePlatforms = false,
+}: OSMLayersProps) => (
+  <>
+    <Background
+      colors={colors[mapStyle]}
+      layerOrder={LAYER_GROUPS_ORDER[LAYERS.BACKGROUND.GROUP]}
+    />
+
+    <Terrain />
+
+    {showOSM && (
+      <>
+        <OSM
+          mapStyle={mapStyle}
+          layerOrder={LAYER_GROUPS_ORDER[LAYERS.BACKGROUND.GROUP]}
+          mapIsLoaded={mapIsLoaded}
+        />
+        <Hillshade mapStyle={mapStyle} layerOrder={LAYER_GROUPS_ORDER[LAYERS.BACKGROUND.GROUP]} />
+      </>
+    )}
+
+    {!hidePlatforms && (
+      <PlatformsLayer
+        colors={colors[mapStyle]}
+        layerOrder={LAYER_GROUPS_ORDER[LAYERS.PLATFORMS.GROUP]}
+      />
+    )}
+
+    <TracksOSM colors={colors[mapStyle]} layerOrder={LAYER_GROUPS_ORDER[LAYERS.TRACKS_OSM.GROUP]} />
+  </>
+);
+
+export default OSMLayers;
