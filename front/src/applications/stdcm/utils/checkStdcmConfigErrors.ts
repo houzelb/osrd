1af78b85db0e741ab037b265ddc79d2036dc68d2
Loup Federico <16464925+Sh099078@users.noreply.github.com>
2025-01-14 10:43:58 +0100

    fixup! front: add new stdcm error when origin and destination are the same

--- front/src/applications/stdcm/utils/checkStdcmConfigErrors.ts
+++ front/src/applications/stdcm/utils/checkStdcmConfigErrors.ts
@@ -30,7 +30,6 @@ const checkStdcmConfigErrors = (
   const isOriginRespectDestinationSchedule =
     origin.arrivalType === ArrivalTimeTypes.RESPECT_DESTINATION_SCHEDULE;
 
-  if (origin.name === destination.name && origin.ch === destination.ch) {
   if (
     'uic' in origin &&
     'uic' in destination &&
